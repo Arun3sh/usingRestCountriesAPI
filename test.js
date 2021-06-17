5fa985b01d1495caf18d063817837a0aac416b8d
Aruneshwaran <65992799+Arun3sh@users.noreply.github.com>
2021-06-18 00:44:10 +0530

    Country name, region and population code

---
+++ test.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+        xhr.onload = function () {
+          var data = JSON.parse(this.response);
+          //console.log(data)
+          for(let i=0; i<data.length; i++){
+            console.log(data[i].name, data[i].region, data[i].subregion, data[i].population);
+          }
+        };
+        xhr.send();
